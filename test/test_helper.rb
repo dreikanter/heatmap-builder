f15203b48035df0cb9cb0b39baf9979e599be9b8
Alex Musayev <alex.musayev@gmail.com>
2025-09-29 12:50:25 +0200

    Enable simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,15 @@
+if ENV["CI"].nil? || ENV["CI"] == "false"
+  require "simplecov"
+
+  SimpleCov.start do
+    add_filter "/test/"
+    minimum_coverage 90
+    formatter SimpleCov::Formatter::HTMLFormatter
+  end
+end
+
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "heatmap-builder"
 
 require "minitest/autorun"
+require "minitest/spec"
